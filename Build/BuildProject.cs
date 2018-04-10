bb84708a51bddfce58bb202a3307664078984ab3
孫御礼 <yuris@premiumartsinc.com>
2018-04-10 12:57:56 +0900

    update build script

--- Build/BuildProject.cs
+++ Build/BuildProject.cs
@@ -1,6 +1,7 @@
 ﻿#if UNITY_EDITOR
 
 using System.IO;
+using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,29 +9,47 @@ using UnityEditor;
 
 public class BuildProject : MonoBehaviour {
 
+	const string bundleid = "com.premiumartsinc.kiryyu";
 	const string outputPath = "Output";
-	const string scenePath = "Scene";
+	const string scenePath = "Scenes";
 
+	static void debugLog(string str) {
+		UnityEngine.Debug.Log (str);
+	}
+
+	static void runCMD(string cmd) {
+		Process.Start (@"cmd", "/c " + cmd);
+	}
 	/*
 	static string[] scenes = new [] { 
 		"Assets/Scene/main.unity" 
 	};
 	*/
 
+	static string firstSceneName = "Assets/Scenes/UI/Splash.unity";
+
 	static string[] findAllScenes() {
 		string folderName = Application.dataPath + "/" + scenePath;
 		var dirInfo = new DirectoryInfo(folderName);
 		var allFileInfos = dirInfo.GetFiles("*.unity", SearchOption.AllDirectories);
 		List<string> scenes = new List<string>();
+		scenes.Add (firstSceneName);
 		foreach (var fileInfo in allFileInfos)
 		{
 			var name = fileInfo.FullName;
 			if (name != null) {
 				scenes.Add (name);
-				Debug.Log ("find scene: " + name);
+				debugLog("find scene: " + name);
 			}
 		}
 		return scenes.ToArray();
+	}
+
+	static void openFolder(string folder) {
+		var rootdir = Directory.GetCurrentDirectory ();
+		var finalpath = Path.Combine (rootdir, folder);
+		debugLog("try to open folder: " + finalpath);
+		Process.Start(@finalpath);
 	}
 
 	[MenuItem("Build/iOS/adhoc")]
@@ -60,7 +79,8 @@ public class BuildProject : MonoBehaviour {
 		buildPlayerOptions.locationPathName = outputPath + "/android_adhoc.apk";
 		buildPlayerOptions.target = BuildTarget.Android;
 		buildPlayerOptions.options = BuildOptions.None;
-		BuildPipeline.BuildPlayer(buildPlayerOptions);
+		BuildPipeline.BuildPlayer (buildPlayerOptions);
+		openFolder (outputPath);
 	}
 
 	[MenuItem("Build/Android/store")]
@@ -73,7 +93,26 @@ public class BuildProject : MonoBehaviour {
 		BuildPipeline.BuildPlayer(buildPlayerOptions);
 	}
 
+	[MenuItem("ADB/uninstall")]
+	public static void BuildADBUninstall() {
+		runCMD("adb uninstall " + bundleid);
+	}
 
+	[MenuItem("ADB/install-adhoc")]
+	public static void BuildADBInstallAdhoc() {
+		var rootdir = Directory.GetCurrentDirectory ();
+		var output = Path.Combine (rootdir, outputPath);
+		var apkpath = Path.Combine (output, "android_adhoc.apk");
+		runCMD ("adb install " + apkpath);
+	}
+
+	[MenuItem("ADB/install-store")]
+	public static void BuildADBInstallStore() {
+		var rootdir = Directory.GetCurrentDirectory ();
+		var output = Path.Combine (rootdir, outputPath);
+		var apkpath = Path.Combine (output, "android_store.apk");
+		runCMD ("adb install " + apkpath);
+	}
 }
 
 #endif
