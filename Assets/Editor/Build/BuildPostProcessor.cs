52307e2c64836c41f5f47c5e6da83a8c2bc1f814
孫御礼 <yuris@premiumartsinc.com>
2018-04-18 18:14:12 +0900

    update

---
+++ Assets/Editor/Build/BuildPostProcessor.cs
@@ -0,0 +1,42 @@
+#if UNITY_EDITOR
+
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+using System.Collections;
+
+public static class BuildPostProcess
+{
+    [PostProcessBuild]
+    public static void OnPostProcessBuild(BuildTarget target, string xcodeProjectPath)
+    {
+		if (target == BuildTarget.iOS) {
+			ProcessForiOS (xcodeProjectPath);
+		}
+    }
+
+	private static void ProcessForiOS (string path)
+	{
+		string pjPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
+		PBXProject pj = new PBXProject ();
+		pj.ReadFromString (File.ReadAllText (pjPath));
+
+		string target = pj.TargetGuidByName ("Unity-iPhone");
+		AddFrameworks (pj, target);
+
+		pj.SetBuildProperty (target, "CLANG_ENABLE_MODULES", "YES");
+
+		File.WriteAllText (pjPath, pj.WriteToString ());
+	}
+
+	private static void AddFrameworks(PBXProject project, string targetGUID)
+	{
+		project.AddFrameworkToProject (project, "WebKit.framework", false);
+		project.AddFrameworkToProject (project, "Photos.framework", false);
+		project.AddFrameworkToProject (project, "PhotosUI.framework", false);
+	}
+}
+
+#endif
